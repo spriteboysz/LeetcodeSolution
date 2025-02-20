6bb98bc862e9089bff47a5a3d7728177353ad28d
spriteboysz <spriteboysz@hotmail.com>
2025-02-20 23:00:32 +0800

    2025022023

--- LeetcodePython/algorithm/P3452. 好数字之和.py
+++ LeetcodePython/algorithm/P3452. 好数字之和.py
@@ -15,14 +15,8 @@ class Solution:
     def sumOfGoodNumbers(self, nums: List[int], k: int) -> int:
         ss = 0
         for i, num in enumerate(nums):
-            if i - k < 0:
-                a = 0
-            else:
-                a = nums[i - k]
-            try:
-                b = nums[i + k]
-            except IndexError:
-                b = 0
+            a = nums[i - k] if i - k >= 0 else 0
+            b = nums[i + k] if i + k < len(nums) else 0
             if num > a and num > b:
                 ss += num
         return ss
