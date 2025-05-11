ee04f5fc30d303ac444b97e440a03c39f6dc0452
Deean <spriteboysz@hotmail.com>
2025-05-11 09:52:15 +0800

    2025051109

---
+++ LeetcodeCC/algorithm/P0001.cpp
@@ -0,0 +1,32 @@
+/**
+ * Author: Deean
+ * Date: 2025-05-11 09:28
+ * FileName: P0001.cpp
+ * Description:
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        const int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+int main() {
+    vector nums = {2, 7, 11, 15};
+    vector ans = Solution().twoSum(nums, 9);
+    cout << ans.size() << endl;
+    return 0;
+}
